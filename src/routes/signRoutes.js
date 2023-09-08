d365a85677b63c166768d6a45686c7047f318cbb
HAMEL Julien <julienhamel.h@gmail.com>
2023-09-08 11:23:53 +0200

    feat: add username field

--- src/routes/signRoutes.js
+++ src/routes/signRoutes.js
@@ -1,25 +1,46 @@
 import jsonwebtoken from "jsonwebtoken"
+import yup from "yup"
+import { prisma } from "../../app.js"
 import config from "../config.js"
 import hashPassword from "../utils/hashPassword.js"
-import { prisma } from "../../app.js"
-import yup from "yup"
 
 const signUpSchema = yup.object().shape({
   email: yup.string().email().required(),
   password: yup.string().min(8).required(),
+  username: yup.string().min(3).required(),
 })
 
 const signRoutes = (app) => {
   app.post("/sign-up", async (req, res) => {
-    const { email, password } = req.body
+    const { email, username, password } = req.body
 
     try {
-      await signUpSchema.validate({ email, password })
+      await signUpSchema.validate({ email, password, username })
+
+      const existingUsername = await prisma.user.findFirst({
+        where: {
+          OR: [
+            {
+              username: {
+                equals: username,
+                mode: "insensitive",
+              },
+            },
+          ],
+        },
+      })
+
+      if (existingUsername) {
+        res.status(500).send({ error: req.t("500") })
+
+        return
+      }
 
       const user = await prisma.user.create({
         data: {
           email,
           passwordHash: hashPassword(password),
+          username,
         },
       })
 
